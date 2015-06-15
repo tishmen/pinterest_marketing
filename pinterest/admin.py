74d68263a2690a741ff0961766d016ef23aacbe1
Milan Topuzov <topuzov.work@gmail.com>
2015-06-16 01:52:09 +0200

    Added admin actions

--- pinterest/admin.py
+++ pinterest/admin.py
@@ -1,9 +1,13 @@
 from django import forms
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.db import models
 
 from pinterest.forms import UserAdminForm
 from pinterest.models import Board, User
+from pinterest.tasks import (
+    create_user_task, confirm_email_task, login_task, create_boards_task,
+    repin_task, like_task, comment_task, follow_task, unfollow_task
+)
 
 
 class BoardInline(admin.StackedInline):
@@ -25,8 +29,83 @@ class UserAdmin(admin.ModelAdmin):
         'name', 'username', 'board_count', 'repin_count', 'like_count',
         'comment_count', 'followers_count', 'following_count'
     )
+    actions = (
+        'create_user_action', 'confirm_email_action', 'login_action',
+        'create_boards_action', 'repin_action', 'like_action',
+        'comment_action', 'follow_action', 'unfollow_action'
+    )
     form = UserAdminForm
     inlines = (BoardInline, )
+
+    def display_message(self, request, task_name, count):
+        '''Display message in list view for admin action.'''
+        if count == 1:
+            message = 'Delayed {} for 1 user.'.format(task_name)
+        else:
+            message = 'Delayed {} for {} users.'.format(task_name, count),
+        self.message_user(request, message, level=messages.SUCCESS)
+
+    def create_user_action(self, request, queryset):
+        '''Admin action for creating pinterest users.'''
+        queryset = queryset.filter(cookies='[]')
+        for user in queryset:
+            create_user_task.delay(user)
+        self.display_message(request, 'create_user_task', queryset.count())
+
+    def confirm_email_action(self, request, queryset):
+        '''Admin action for confirming pinterest email.'''
+        queryset = queryset.exclude(cookies='[]')
+        for user in queryset:
+            confirm_email_task.delay(user)
+        self.display_message(request, 'confirm_email_task', queryset.count())
+
+    def create_boards_action(self, request, queryset):
+        '''Admin action for creating pinterest boards.'''
+        queryset = queryset.exclude(cookies='[]')
+        for user in queryset:
+            create_boards_task.delay(user)
+        self.display_message(request, 'create_boards_task', queryset.count())
+
+    def login_action(self, request, queryset):
+        '''Admin action for loging pinterest users.'''
+        for user in queryset:
+            login_task.delay(user)
+        self.display_message(request, 'login_task', queryset.count())
+
+    def like_action(self, request, queryset):
+        '''Admin action for liking random pinterest pins.'''
+        queryset = queryset.exclude(cookies='[]')
+        for user in queryset:
+            like_task.delay(user)
+        self.display_message(request, 'like_task', queryset.count())
+
+    def comment_action(self, request, queryset):
+        '''Admin action for commenting on random pinterest pins.'''
+        queryset = queryset.exclude(cookies='[]')
+        for user in queryset:
+            comment_task.delay(user)
+        self.display_message(request, 'like_task', queryset.count())
+
+    def repin_action(self, request, queryset):
+        '''Admin action for repinning random pinterest pins.'''
+        queryset = queryset.exclude(cookies='[]')
+        for user in queryset:
+            repin_task.delay(user)
+        self.display_message(request, 'repin_task', queryset.count())
+
+    def follow_action(self, request, queryset):
+        '''Admin action for following random pinterest users.'''
+        queryset = queryset.exclude(cookies='[]')
+        for user in queryset:
+            follow_task.delay(user)
+        self.display_message(request, 'follow_task', queryset.count())
+
+    def unfollow_action(self, request, queryset):
+        '''Admin action for unfollowing random pinterest users.'''
+        queryset = queryset.exclude(cookies='[]')
+        for user in queryset:
+            unfollow_task.delay(user)
+        self.display_message(request, 'unfollow_task', queryset.count())
 
     def get_queryset(self, request):
         '''Override model admin get_queryset. Return annotated queryset.'''
@@ -43,36 +122,47 @@ class UserAdmin(admin.ModelAdmin):
         '''Return user board count.'''
         return user.board_count
 
-    board_count.admin_order_field = 'board_count'
-
     def repin_count(self, user):
         '''Return user repin count.'''
         return user.repin_count
-
-    repin_count.admin_order_field = 'repin_count'
 
     def like_count(self, user):
         '''Return user like count.'''
         return user.like_count
 
-    like_count.admin_order_field = 'like_count'
-
     def comment_count(self, user):
         '''Return user comment count.'''
         return user.comment_count
-
-    comment_count.admin_order_field = 'comment_count'
 
     def followers_count(self, user):
         '''Return user followers count.'''
         return user.followers_count
 
-    followers_count.admin_order_field = 'followers_count'
-
     def following_count(self, user):
         '''Return user following count.'''
         return user.following_count
 
+    create_user_action.short_description = (
+        'Create user task for selected users'
+    )
+    confirm_email_action.short_description = (
+        'Confirm email task for selected users'
+    )
+    login_action.short_description = 'Login task for selected users'
+    create_boards_action.short_description = (
+        'Create boards task for selected users'
+    )
+    like_action.short_description = 'Like task for selected users'
+    comment_action.short_description = 'Comment task for selected users'
+    repin_action.short_description = 'Repin task for selected users'
+    follow_action.short_description = 'Follow task for selected users'
+    unfollow_action.short_description = 'Unfollow task for selected users'
+
+    board_count.admin_order_field = 'board_count'
+    repin_count.admin_order_field = 'repin_count'
+    like_count.admin_order_field = 'like_count'
+    comment_count.admin_order_field = 'comment_count'
+    followers_count.admin_order_field = 'followers_count'
     following_count.admin_order_field = 'following_count'
 
 
